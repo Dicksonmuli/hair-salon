b4b516abf36ec87b4132834f67ba73b2cf2e239b
dicksonmuli <dicksonmully6@gmail.com>
2017-05-04 16:47:51 +0300

    add database rule file

---
+++ src/test/java/StylistTest.java
@@ -0,0 +1,11 @@
+import java.time.LocalDateTime;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.sql2o.*;
+
+public class StylistTest {
+
+	@Rule
+  public DatabaseRule database = new DatabaseRule();
+
+}

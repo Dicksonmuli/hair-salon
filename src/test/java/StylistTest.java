59fc2cc75953862fc470e48b8105036cd185729e
dicksonmuli <dicksonmully6@gmail.com>
2017-05-04 17:05:27 +0300

    first test- check if instantiates successfuly

--- src/test/java/StylistTest.java
+++ src/test/java/StylistTest.java
@@ -7,5 +7,17 @@ public class StylistTest {
 
 	@Rule
   public DatabaseRule database = new DatabaseRule();
+	//defining an object of a stylists
+	private Stylist testStylist;
+	@Before
+	public void instance() {
+		testStylist = new Stylist(1, "Yvone", 254719364087, "works well");
+	}
 
+	//creating an instance of Stylist
+	//checks if the instance is created successfully
+	@Test
+	public void Task_instantiatesCorrectly_true() {
+		assertTrue(testStylist instanceof Stylist0);
+	}
 }

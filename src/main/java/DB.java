3026bdb8d5e29ea93d1f24a19e1f1c0d7069a88a
dicksonmuli <dicksonmully6@gmail.com>
2017-05-05 18:22:33 +0300

    add heroku database name

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -2,5 +2,5 @@ import org.sql2o.*;
 
 public class DB {
   public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon", "dickson", "dickson");
-  // public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-235-120-39.compute-1.amazonaws.com:5432/d5puid2u1ldeqm", "lbxmmgtzlerudc", "8a79283ee338552c5aa41d3876df680467652a19ccbf7faf541177029f7bdde1");
+  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-107-20-163-238.compute-1.amazonaws.com:5432/d405hr1s39001c", "stuptizabijgno", "0628655107bec7f7736dcde1d05b5460ef93a07e85b30ca2b3498f083a477f15");
 }
